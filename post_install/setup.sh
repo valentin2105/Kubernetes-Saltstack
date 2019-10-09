536c08a693ee5dc3a5145a183dd3f8094a8a81ff
Valentin Ouvrard <valentin@ouvrard.it>
2019-10-09 15:16:06 +1100

    coredns Fix.

--- post_install/setup.sh
+++ post_install/setup.sh
@@ -9,20 +9,20 @@ sleep 10
 kubectl create -f coredns.yaml
 kubectl create -f kubernetes-dashboard.yaml
 
-kubectl create -f heapster-rbac.yaml
-kubectl create -f influxdb.yaml
-kubectl create -f grafana.yaml
-kubectl create -f heapster.yaml
+#kubectl create -f heapster-rbac.yaml
+#kubectl create -f influxdb.yaml
+#kubectl create -f grafana.yaml
+#kubectl create -f heapster.yaml
 
-wget https://kubernetes-helm.storage.googleapis.com/helm-{{ HELM_VERSION }}-linux-amd64.tar.gz
-tar -zxvf helm-{{ HELM_VERSION }}-linux-amd64.tar.gz
-mv linux-amd64/helm /usr/local/bin/helm
-rm -r linux-amd64/ && rm -r helm-{{ HELM_VERSION }}-linux-amd64.tar.gz
+#wget https://kubernetes-helm.storage.googleapis.com/helm-{{ HELM_VERSION }}-linux-amd64.tar.gz
+#tar -zxvf helm-{{ HELM_VERSION }}-linux-amd64.tar.gz
+#mv linux-amd64/helm /usr/local/bin/helm
+#rm -r linux-amd64/ && rm -r helm-{{ HELM_VERSION }}-linux-amd64.tar.gz
 
 kubectl create serviceaccount tiller --namespace kube-system
 
-kubectl create -f rbac-tiller.yaml
-helm init --service-account tiller
+#kubectl create -f rbac-tiller.yaml
+#helm init --service-account tiller
 
 sleep 2
 echo ""
